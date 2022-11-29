1303ee4a9f0e043a99007c2dae549fd94d2b6c9a
Aleksandr Kondratov <al.kon.workmail@gmail.com>
2022-11-29 12:02:14 +0200

    price added on product item

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./ProductItem.module.scss";
 import { Link } from "react-router-dom";
 
-const ProductItem = ({ name, description, photo, id }) => {
+const ProductItem = ({ name, description, photo, id, price }) => {
   return (
     <Link to={`/products/${id}`} className={styles.container}>
       <img src={photo} alt={name} className={styles.photo} />
       <div className={styles.text}>
         <h4>{name}</h4>
         <p>{description}</p>
+        <p className={styles.price}>{price} грн</p>
       </div>
     </Link>
   );
